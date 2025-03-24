bcb21e3e20c49f0c4154773ba6de8260e24578e7
Artur <metallist15431543@gmail.com>
2025-03-24 18:34:13 +0300

    add suggestion list to CountrySearch component

--- src/pages/CountrySearch/CountrySearch.tsx
+++ src/pages/CountrySearch/CountrySearch.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from "react";
-import { useGetCountryByNameQuery } from "@api/countries";
+import { useGetAllCountriesQuery, useGetCountryByNameQuery } from "@api/countries";
 import { Input } from "@ui/Input";
 import { Button } from "@ui/Button";
 
@@ -8,21 +8,40 @@ import s from "./CountrySearch.module.css";
 export const CountrySearch = () => {
   const [name, setName] = useState<string>("");
   const [skip, setSkip] = useState(true);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
 
+  const { data: allCountries } = useGetAllCountriesQuery();
   const { data } = useGetCountryByNameQuery(name, {
     skip: skip,
   });
 
+  const country = data?.data;
+
   const onChangeName = (event: ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
+    const value = event.target.value;
+    setName(value);
     setSkip(true);
+
+    if (value.length > 1 && Array.isArray(allCountries?.data)) {
+      const filtered = allCountries.data
+        .filter((country) => country.name.toLowerCase().includes(value.toLowerCase()))
+        .map((country) => country.name);
+      setSuggestions(filtered);
+    } else {
+      setSuggestions([]);
+    }
   };
 
   const handleSearch = () => {
     setSkip(false);
+    setSuggestions([]);
   };
 
-  const country = data?.data;
+  const handleSelectSuggestion = (suggestion: string) => {
+    setName(suggestion);
+    setSkip(false);
+    setSuggestions([]);
+  };
 
   return (
     <div className={s.main}>
@@ -31,6 +50,20 @@ export const CountrySearch = () => {
         <Input value={name} onChange={onChangeName} />
         <Button name={"Search"} onClick={handleSearch} />
       </div>
+
+      {suggestions.length > 0 && (
+        <ul className={s.suggestions}>
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              className={s.suggestionItem}
+              onClick={() => handleSelectSuggestion(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
 
       <div className={s.infoBlock}>
         {country ? (
